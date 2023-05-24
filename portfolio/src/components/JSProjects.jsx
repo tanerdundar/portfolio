40c5cd9ad067bf55e3f1463c5f1ed7dcb830a68d
tanerdundar <t.dundar145@gmail.com>
2023-05-24 16:17:29 +0300

    finish all except style-color balance

--- portfolio/src/components/JSProjects.jsx
+++ portfolio/src/components/JSProjects.jsx
@@ -8,19 +8,19 @@ function JSProjects() {
       </div>
       <div className="single-project">
         <Project
-          photo="./photo/box-move.jpg"
+          photo="https://raw.githubusercontent.com/tanerdundar/portfolio/master/portfolio/src/photo/box-move.jpg"
           site="https://box-mover.netlify.app/"
           name="box-mover"
           link="https://github.com/tanerdundar/boxMover"
         />
         <Project
-          photo="./photo/box-move.JPG"
+          photo="https://raw.githubusercontent.com/tanerdundar/portfolio/master/portfolio/src/photo/mine.JPG"
           site="https://mine-sweeper-ofa.netlify.app/"
           name="mine-sweeper game"
           link="https://github.com/tanerdundar/minesweeper"
         />
         <Project
-          photo="./photo/box-move.JPG"
+          photo="https://raw.githubusercontent.com/tanerdundar/portfolio/master/portfolio/src/photo/mvhesap.JPG"
           site="https://mvcalculate.netlify.app/"
           name="milletvekili hesaplama"
           link="https://github.com/tanerdundar/mvhesaplama"
